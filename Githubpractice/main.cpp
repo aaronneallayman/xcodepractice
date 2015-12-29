235cf3bb38147db3aec66552a5fc4537da8f7b0d
Aaron Layman <aaronlayman@aarons-macbook-air.local>
2015-12-29 02:18:31 -0500

    This is simply testing how the commit function works on xcode.

--- Githubpractice/main.cpp
+++ Githubpractice/main.cpp
@@ -1,15 +1,8 @@
-//
-//  main.cpp
-//  Githubpractice
-//
-//  Created by Aaron Layman on 12/29/15.
-//  Copyright © 2015 Aaron Layman. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main(){
+    cout<<"hello world";
+    cin.ignore();
 }
