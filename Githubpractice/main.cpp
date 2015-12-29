70f5b357fb782af7c47ea290ea550e32a6b877e5
Aaron Layman <aaronlayman@aarons-macbook-air.local>
2015-12-29 11:57:55 -0500

    Added addition and subtraction functionality

--- Githubpractice/main.cpp
+++ Githubpractice/main.cpp
@@ -5,4 +5,23 @@ using namespace std;
 int main(){
     cout<<"hello world";
     cin.ignore();
+    int x, y;
+    cout<<"Enter two numbers";
+    cin>>x>>y;
+    int operation;
+    cout<<"1)Add or 2)subtract";
+    cin>>operation;
+
+    switch(operation){
+        case 1:
+            cout << x+y;
+            break;
+        case 2:
+            cout<< x-y;
+            break;
+        default:
+        cout<<"Error: not an operation";
+            break;
+    }
+    
 }
